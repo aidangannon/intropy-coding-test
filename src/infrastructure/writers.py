e33a1cfd3ff6922ac8394e5462f0807bf8a6e682
aidan <aidanwilliamgannon@gmail.com>
2025-08-06 21:29:15 +0100

    Add logging scope to seed operation

--- src/infrastructure/writers.py
+++ src/infrastructure/writers.py
@@ -2,7 +2,7 @@ from sqlalchemy import exists, select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core import MetricConfiguration
-from src.crosscutting import auto_slots, Logger
+from src.crosscutting import auto_slots, Logger, logging_scope
 
 
 @auto_slots
@@ -15,14 +15,15 @@ class SqlAlchemyGenericDataSeeder:
         """
         seeds the table if the table is not already empty
         """
-        logger.info(f"{len(data)} rows entered")
-        stmt = select(func.count()).select_from(_type.__table__)
+        with logging_scope({"operation": "db_seed"}):
+            logger.info(f"{len(data)} input rows")
+            stmt = select(func.count()).select_from(_type.__table__)
 
-        result = await self.session.execute(stmt)
-        count = result.scalar()
+            result = await self.session.execute(stmt)
+            count = result.scalar()
 
-        logger.info(f"{count} rows found")
-        if count > 0:
-            return
+            logger.info(f"{count} rows found in db")
+            if count > 0:
+                return
 
-        self.session.add_all(data)
+            self.session.add_all(data)
