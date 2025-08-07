3da8e08b9fe6876c0e2bdcba11f3c3be1bd9cc86
aidan <aidanwilliamgannon@gmail.com>
2025-08-07 01:30:18 +0100

    Redirect root to swagger

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from src.bootstrap import bootstrap
 from src.web import lifespan
@@ -6,6 +7,10 @@ from src.web import lifespan
 app = FastAPI(title="Metrics API", version="0.1.0", lifespan=lifespan)
 
 bootstrap(app=app)
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     import uvicorn
